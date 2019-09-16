d38d0ab90c1164e7816de93351ea5dadb4ee2e30
Manu Bhat <mbhat@instartlogic.com>
2019-09-16 14:50:33 -0700

    Add code to simulate reading form fields

---
+++ demo/read-input.js
@@ -0,0 +1,6 @@
+(function () {
+    var elements = document.getElementsByTagName('input');
+    for (var i = 0; i < elements.length; i++) {
+        console.log("Found " + elements[i].value);
+    }
+});
