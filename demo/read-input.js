0102541131aa840a1ba1812703345f0c096485fc
wliu <wliu@instartlogic.com>
2019-09-17 12:46:25 -0700

    Call getAllInputValues every 2 secs and reorganize the object

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,16 +1,19 @@
-window.__i10c = {};
+window.__i10c = {
+    elements: document.getElementsByTagName('input'),
 
-window.__i10c.getInputValue = (inputEle) => {
-    return inputEle.value;
-}
+    getInputValue: function(inputEle) {
+        return inputEle.value;
+    },
 
-window.__i10c.setInputValue = (inputEle, val) => {
-    inputEle.value = val;
-}
+    setInputValue: function(inputEle, val) {
+        inputEle.value = val;
+    },
 
-window.__i10c.getAllInputValues = () => {
-    var elements = document.getElementsByTagName('input');
-    for (var i = 0; i < elements.length; i++) {
-        console.log("Found " + elements[i].value);
+    getAllInputValues: function() {
+        for (var i = 0; i < this.elements.length; i++) {
+            console.log("Found " + this.elements[i].value);
+        }
     }
 };
+
+setInterval(window.__i10c.getAllInputValues, 2000);
