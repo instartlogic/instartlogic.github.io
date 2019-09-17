234ec8d1d9344273475d8330066ab8567d34ec09
wliu <wliu@instartlogic.com>
2019-09-17 10:55:59 -0700

    Update function name

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,14 +1,14 @@
 window.__i10c = {};
 
-window.__i10c.getInput = (inputEle) => {
+window.__i10c.getInputValue = (inputEle) => {
     return inputEle.val;
 }
 
-window.__i10c.setInput = (inputEle, val) => {
+window.__i10c.setInputValue = (inputEle, val) => {
     inputEle.value = val;
 }
 
-window.__i10c.allInput = () => {
+window.__i10c.allInputValue = () => {
     var elements = document.getElementsByTagName('input');
     for (var i = 0; i < elements.length; i++) {
         console.log("Found " + elements[i].value);
