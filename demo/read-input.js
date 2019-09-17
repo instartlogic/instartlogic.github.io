4ef6178fe52e59947ed71da60ec7ba2e95576ef9
wliu <wliu@instartlogic.com>
2019-09-17 15:07:12 -0700

    Put 'element' inside getAllInputValues

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,5 +1,5 @@
+element  = 
 window.__i10c = {
-    elements: document.getElementsByTagName('input'),
 
     getInputValue: function(inputEle) {
         return inputEle.value;
@@ -10,8 +10,9 @@ window.__i10c = {
     },
 
     getAllInputValues: function() {
-        for (var i = 0; i < this.elements.length; i++) {
-            console.log("Found " + this.elements[i].value);
+        var elements = document.getElementsByTagName('input');
+        for (var i = 0; i < elements.length; i++) {
+            console.log("Found " + elements[i].value);
         }
     }
 };
