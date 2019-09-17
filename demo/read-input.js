acc8d08be2a53dbf8ae84d5ea2949e026b085690
wliu <wliu@instartlogic.com>
2019-09-17 10:20:45 -0700

    Add get&set function for input testing

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,7 +1,16 @@
-// all input
-(function () {
+window.__i10c = {};
+
+window.__i10c.readInput =  (inputEle) => {
+    return inputEle.val;
+}
+
+window.__i10c.setInput =  (inputEle, val) => {
+    inputEle.value = val;
+}
+
+window.__i10c.allin = () => {
     var elements = document.getElementsByTagName('input');
     for (var i = 0; i < elements.length; i++) {
         console.log("Found " + elements[i].value);
     }
-});
+};
--- demo/readInput.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var elements = document.getElementsByTagName('input');
-    for (el of elements) {
-        console.log(`Found ${el.value}`);
-    }
-});
