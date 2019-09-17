25803685d7feb1cd079c5bf29a5c30bf8cd90d1c
wliu <wliu@instartlogic.com>
2019-09-17 15:45:42 -0700

    Format

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,14 +1,14 @@
 window.__i10c = {
 
-    getInputValue: function(inputEle) {
+    getInputValue: function (inputEle) {
         return inputEle.value;
     },
 
-    setInputValue: function(inputEle, val) {
+    setInputValue: function (inputEle, val) {
         inputEle.value = val;
     },
 
-    getAllInputValues: function() {
+    getAllInputValues: function () {
         var elements = document.getElementsByTagName('input');
         for (var i = 0; i < elements.length; i++) {
             console.log("Found " + elements[i].value);
