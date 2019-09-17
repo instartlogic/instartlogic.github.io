cce7d2b09c0b50300a5c85eefd80f53bae39e7a1
wliu <wliu@instartlogic.com>
2019-09-17 12:25:16 -0700

    Fix name and bug for get input value

--- demo/read-input.js
+++ demo/read-input.js
@@ -1,14 +1,14 @@
 window.__i10c = {};
 
 window.__i10c.getInputValue = (inputEle) => {
-    return inputEle.val;
+    return inputEle.value;
 }
 
 window.__i10c.setInputValue = (inputEle, val) => {
     inputEle.value = val;
 }
 
-window.__i10c.allInputValue = () => {
+window.__i10c.getAllInputValues = () => {
     var elements = document.getElementsByTagName('input');
     for (var i = 0; i < elements.length; i++) {
         console.log("Found " + elements[i].value);
